4033423a76f41d07616e9761cc7464e91c7c65e6
Michael Tu <tuzhucheng@outlook.com>
2014-07-11 19:32:41 -0400

    heroku port

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.get('/', function(req, res){
     res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+var port = Number(process.env.PORT || 3000);
+http.listen(port, function(){
+    console.log('server is listening');
 });
